1c25362e32849dab5ea256f7fab4610eb1a7ef38
high5digital <kallal.nsu103@gmail.com>
2019-08-13 02:13:49 +0600

    Updates

---
+++ precache-manifest.df6a71e4e539ea71a2a6f84124b539a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef96b06f37156ae096cf0b3b3be82c4a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b5e97fb8877b29150dda",
+    "url": "/monsters-rolodex/static/css/main.9f66e737.chunk.css"
+  },
+  {
+    "revision": "63a9fc22d2c0021a11c2",
+    "url": "/monsters-rolodex/static/js/2.4b270b07.chunk.js"
+  },
+  {
+    "revision": "b5e97fb8877b29150dda",
+    "url": "/monsters-rolodex/static/js/main.b90aa58d.chunk.js"
+  },
+  {
+    "revision": "4e8a8e8c497e779916d5",
+    "url": "/monsters-rolodex/static/js/runtime~main.fb49a3bc.js"
+  }
+]);
