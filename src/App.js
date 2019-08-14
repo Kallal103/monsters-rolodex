b378de313b968edb7f20db55f3d4f92f442309f2
high5digital <kallal.nsu103@gmail.com>
2019-08-14 18:32:41 +0600

    add masters file

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+import './App.css';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+     monsters: [],
+     searchField: ''
+    };
+
+   
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json() )
+    .then(users => this.setState({monsters: users}));
+  }
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value});
+
+  }
+  render(){
+    const {monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monsters =>
+       monsters.name.toLowerCase().includes(searchField.toLocaleLowerCase()
+       ));
+    return(
+      <div className="App">
+        <h1>Monsters Roledex</h1>
+       <SearchBox
+       placeholder='search Monsters'
+       handleChange={this.handleChange}
+       />
+        <CardList monsters={filteredMonsters}/>
+       
+        
+      
+    </div>
+    )
+  }
+}
+
+export default App;
